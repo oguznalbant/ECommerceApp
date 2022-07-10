6b95ecfd4f023c029e679237534a3a634142a5f9
Oguzhan Nalbant <oguz.nalbant@gmail.com>
2022-07-10 21:51:22 +0300

    basket microservices api and repo development

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,15 +1,24 @@
+using Basket.API.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container. Dependecy injection part
+
+// it provides IDistrbutedCache interface serves the redis implementatiton.
+builder.Services.AddStackExchangeRedisCache(o => o.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString"));
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline. Middleware part
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
