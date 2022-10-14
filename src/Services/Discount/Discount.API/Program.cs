3b291a0b0291a66e10e4b803b528ab041094111f
Oguzhan Nalbant <oguz.nalbant@gmail.com>
2022-10-14 23:47:14 +0300

    discount repository inject, and discount api containerization

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,7 +1,10 @@
+using Discount.API.Repository;
+using Discount.API.Repository.Abstraction;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
